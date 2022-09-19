4625ca27d4d49ba1a02fdc691929af1282f80d27
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2022-09-19 21:17:51 +0300

    update: keymap

--- strash/keymap.c
+++ strash/keymap.c
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	//|-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+---------------|
 	            KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,            KC_Y,         KC_U,         KC_I,         KC_O, TD(QWE_P_QUOT),
 	//|-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+---------------|
-	    LCTL_T(KC_A), LOPT_T(KC_S), LSFT_T(KC_D), LCMD_T(KC_F),         KC_G,            KC_H, RCMD_T(KC_J), RSFT_T(KC_K), ROPT_T(KC_L),RCTL_T(KC_SCLN),
+	            KC_A,         KC_S,         KC_D,         KC_F,         KC_G,            KC_H,         KC_J,         KC_K,         KC_L,        KC_SCLN,
 	//|-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+---------------|
 	            KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,            KC_N,         KC_M,      KC_COMM,       KC_DOT,        KC_SLSH,
 	//|-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+---------------|
@@ -154,6 +154,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 				if (IS_LAYER_ON(NAV)) layer_off(NAV);
 				if (timer_elapsed(timer_base_nav) < TAPPING_TERM) {
 					default_layer_xor((DVO + 1) | (QWE + 1));
+					register_mods(MOD_MASK_CSAG);
+					register_code(KC_SPC);
+					clear_keyboard();
 				}
 			}
 			return false;
