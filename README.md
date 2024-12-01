49de65703d1fa992ffe94ba318140b1035c3dbb2
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2024-12-02 02:17:31 +0300

    update
    - Add a combo to activate Caps Word (Smart Shift). This way, you don't
      have to wait for Shift to be activated. Holding Shift still works
      though.
    - Unmap all the inner thumbs and move their bindings to the middle
      thumbs.
    - Add Shift to the QWERTY layer.
    - Add Tap Dance to the missing keys on the QWERTY layer:
      ь -> х, б -> ъ, / -> ё.
    - Add Shift+Tab to the Navigation layer and move Tab to the right.
    - Remove the home row mode from the Symbols layer.
    - Replace ` with Tap Dance: ~ -> `.
    - Replace + with Tap Dance: + -> =.
    - Replace = with _.
    - Rearrange the Media keys and add the Esc button to the Media layer.
    - Update image in the README.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # QMK 3x5+3
 
 ## Based on `crkbd` keyboard.
-![5x3+3](https://github.com/strash/qmk_3x5_3/assets/14233263/e9974a4a-2eca-4eed-9f7d-d29f8f07f672)
+![5x3+3](https://github.com/user-attachments/assets/4be28a7f-c026-43b9-bf06-cccf1963895b)
--- crkbd/strash/keymap.c
+++ crkbd/strash/keymap.c
@@ -39,12 +39,39 @@ enum key {
 bool is_select_prev_app_active = false;
 bool is_select_next_app_active = false;
 
+uint16_t nav_layer_timer = 0;
+uint16_t sym_layer_timer = 0;
+
+// tap dance
+
 enum tap_dance {
+	TILD_GRV,
+	PLUS_EQL,
 	QWE_P_QUOT,
+	QWE_M_LBRC,
+	QWE_COMM_RBRC,
+	QWE_SLSH_BSLS,
 };
 
 tap_dance_action_t tap_dance_actions[] = {
-	[QWE_P_QUOT] = ACTION_TAP_DANCE_DOUBLE(KC_P, KC_QUOT),
+	[TILD_GRV]      = ACTION_TAP_DANCE_DOUBLE(KC_TILD, KC_GRV),
+	[PLUS_EQL]       = ACTION_TAP_DANCE_DOUBLE(KC_PLUS, KC_EQL),
+	[QWE_P_QUOT]    = ACTION_TAP_DANCE_DOUBLE(KC_P,    KC_QUOT),
+	[QWE_M_LBRC]    = ACTION_TAP_DANCE_DOUBLE(KC_M,    KC_LBRC),
+	[QWE_COMM_RBRC] = ACTION_TAP_DANCE_DOUBLE(KC_COMM, KC_RBRC),
+	[QWE_SLSH_BSLS] = ACTION_TAP_DANCE_DOUBLE(KC_SLSH, KC_BSLS),
+};
+
+// combo
+
+enum combo_event {
+	DVO_CAPS_WORD,
+};
+
+const uint16_t PROGMEM dvo_caps_word_combo[] = {LSFT_T(KC_E), RSFT_T(KC_T), COMBO_END};
+
+combo_t key_combos[] = {
+	[DVO_CAPS_WORD] = COMBO_ACTION(dvo_caps_word_combo),
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -57,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	//|-------------+-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+-------------+-------------|
              XXXXXXX,      KC_SCLN,         KC_Q,         KC_J,         KC_K,         KC_X,            KC_B,         KC_M,         KC_W,         KC_V,         KC_Z,      XXXXXXX, \
 	//|-------------+-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+-------------+-------------|
-														KC_ESC,     NAV_HOLD,LCTL_T(KC_SPC), RCTL_T(KC_ENT),     SYM_HOLD,      KC_BSPC \
+													   XXXXXXX,     NAV_HOLD,LCTL_T(KC_SPC), RCTL_T(KC_ENT),     SYM_HOLD,      XXXXXXX \
 											  //|-------------+-------------+-------------|  |-------------+-------------+-------------|
 	),
 
@@ -69,11 +96,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	//|-------------+-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+-------------+-------------|
 			 XXXXXXX,         KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,            KC_Y,         KC_U,         KC_I,         KC_O,TD(QWE_P_QUOT),     XXXXXXX, \
 	//|-------------+-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+-------------+-------------|
-			 XXXXXXX,         KC_A,         KC_S,         KC_D,         KC_F,         KC_G,            KC_H,         KC_J,         KC_K,         KC_L,      KC_SCLN,      XXXXXXX, \
+			 XXXXXXX,         KC_A,         KC_S, LSFT_T(KC_D),         KC_F,         KC_G,            KC_H,         KC_J, RSFT_T(KC_K),         KC_L,      KC_SCLN,      XXXXXXX, \
 	//|-------------+-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+-------------+-------------|
-			 XXXXXXX,         KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,            KC_N,         KC_M,      KC_COMM,       KC_DOT,      KC_SLSH,      XXXXXXX, \
+			 XXXXXXX,         KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,            KC_N,TD(QWE_M_LBRC),TD(QWE_COMM_RBRC),  KC_DOT,TD(QWE_SLSH_BSLS),  XXXXXXX, \
 	//|-------------+-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+-------------+-------------|
-														KC_ESC,     NAV_HOLD,       KC_SPC,          KC_ENT,     SYM_HOLD,LSFT_T(KC_BSPC) \
+													   XXXXXXX,     NAV_HOLD,       KC_SPC,          KC_ENT,     SYM_HOLD,      XXXXXXX \
 											  //|-------------+-------------+-------------|  |-------------+-------------+-------------|
 	),
 
@@ -81,11 +108,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	//|-------------+-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+-------------+-------------|
 			 XXXXXXX,     HIS_BACK,     HIS_FORW,     PREV_TAB,     NEXT_TAB,LCMD(KC_BSPC),    RSG(KC_LEFT),RSFT(KC_LEFT),RSFT(KC_DOWN),  RSFT(KC_UP),RSFT(KC_RGHT),      XXXXXXX, \
 	//|-------------+-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+-------------+-------------|
-			 XXXXXXX,       KC_TAB,     KC_LOPT,   KC_LSFT,         KC_LCMD,LOPT(KC_BSPC),   RCMD(KC_LEFT),      KC_LEFT,      KC_DOWN,        KC_UP,      KC_RGHT,      XXXXXXX, \
+			 XXXXXXX, LSFT(KC_TAB),       KC_TAB,      KC_LSFT,      KC_LCMD,LOPT(KC_BSPC),   RCMD(KC_LEFT),      KC_LEFT,      KC_DOWN,        KC_UP,      KC_RGHT,      XXXXXXX, \
 	//|-------------+-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+-------------+-------------|
 			 XXXXXXX,     PREV_APP,     NEXT_APP,     PREW_SPC,     NEXT_SPC, LOPT(KC_DEL),    RSA(KC_LEFT),ROPT(KC_LEFT),ROPT(KC_DOWN),  ROPT(KC_UP),ROPT(KC_RGHT),      XXXXXXX, \
 	//|-------------+-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+-------------+-------------|
-													   XXXXXXX,      XXXXXXX,      XXXXXXX,            LANG,         BASE,      KC_BSPC \
+													   XXXXXXX,      XXXXXXX,      XXXXXXX,            LANG,         BASE,      XXXXXXX \
 											  //|-------------+-------------+-------------|  |-------------+-------------+-------------|
 	),
 
@@ -93,23 +120,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	//|-------------+-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+-------------+-------------|
 			 XXXXXXX,      KC_EXLM,        KC_AT,      KC_HASH,       KC_DLR,      KC_PERC,         KC_CIRC,      KC_AMPR,      KC_ASTR,      KC_LPRN,      KC_RPRN,      XXXXXXX, \
 	//|-------------+-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+-------------+-------------|
-			 XXXXXXX,LCTL_T(KC_P1),LOPT_T(KC_P2),LSFT_T(KC_P3),LCMD_T(KC_P4),        KC_P5,           KC_P6,RCMD_T(KC_P7),RSFT_T(KC_P8),ROPT_T(KC_P9),RCTL_T(KC_P0),      XXXXXXX, \
+			 XXXXXXX,        KC_P1,        KC_P2,        KC_P3,        KC_P4,        KC_P5,           KC_P6,        KC_P7,        KC_P8,        KC_P9,        KC_P0,      XXXXXXX, \
 	//|-------------+-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+-------------+-------------|
-			 XXXXXXX,      KC_LCBR,      KC_RCBR,      KC_MINS,      KC_PLUS,       KC_GRV,         KC_SLSH,      KC_LBRC,      KC_RBRC,      KC_PIPE,      KC_BSLS,      XXXXXXX, \
+			 XXXXXXX,      KC_LCBR,      KC_RCBR,      KC_MINS, TD(PLUS_EQL), TD(TILD_GRV),         KC_SLSH,      KC_LBRC,      KC_RBRC,      KC_PIPE,      KC_BSLS,      XXXXXXX, \
 	//|-------------+-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+-------------+-------------|
-													   KC_UNDS,       KC_EQL,    TO(MEDIA),         XXXXXXX,      XXXXXXX,      XXXXXXX \
+													   XXXXXXX,      KC_UNDS,    TO(MEDIA),         XXXXXXX,      XXXXXXX,      XXXXXXX \
 											  //|-------------+-------------+-------------|  |-------------+-------------+-------------|
 	),
 
 	[MEDIA] = LAYOUT( \
 	//|-------------+-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+-------------+-------------|
-			 XXXXXXX,WEB_INSPECTOR,      XXXXXXX,        VIDEO,     SCR_TO_F,     SCR_TO_B,         XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_MPLY,      XXXXXXX, \
+			 XXXXXXX,WEB_INSPECTOR,      XXXXXXX,        VIDEO,     SCR_TO_F,     SCR_TO_B,          KC_ESC,      KC_MPLY,      KC_MUTE,      XXXXXXX,      KC_MPLY,      XXXXXXX, \
 	//|-------------+-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+-------------+-------------|
-			 XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_BRMD,      KC_BRMU,   LCMD(KC_C),         KC_MUTE,      KC_MRWD,      KC_VOLD,      KC_VOLU,      KC_MFFD,      XXXXXXX, \
+			 XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_BRMD,      KC_BRMU,   LCMD(KC_C),         KC_BSPC,      KC_MRWD,      KC_VOLD,      KC_VOLU,      KC_MFFD,      XXXXXXX, \
 	//|-------------+-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+-------------+-------------|
 			 XXXXXXX,      KC_LEFT,      KC_DOWN,        KC_UP,      KC_RGHT,   LCMD(KC_V),         XXXXXXX,      KC_LEFT,      KC_DOWN,        KC_UP,      KC_RGHT,      XXXXXXX, \
 	//|-------------+-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+-------------+-------------|
-														KC_ESC,     NAV_HOLD,       KC_SPC,          KC_ENT,     SYM_HOLD,      KC_BSPC \
+													   XXXXXXX,     NAV_HOLD,       KC_SPC,          KC_ENT,     SYM_HOLD,      XXXXXXX \
 											  //|-------------+-------------+-------------|  |-------------+-------------+-------------|
 	),
 };
@@ -125,6 +152,16 @@ void unselect_app_selection(void) {
 	}
 };
 
+void process_combo_event(uint16_t combo_index, bool is_pressed) {
+	switch (combo_index) {
+		case DVO_CAPS_WORD:
+			if (is_pressed && layer_state_is(DVO)) {
+				caps_word_on();
+			}
+			break;
+	}
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
 		case BASE:
@@ -137,20 +174,30 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 			return false;
 		case NAV_HOLD:
 			if (record->event.pressed) {
+				nav_layer_timer = timer_read();
 				if (IS_LAYER_ON(MEDIA)) layer_off(MEDIA);
 				if (IS_LAYER_OFF(NAV)) layer_on(NAV);
 			} else {
 				unselect_app_selection();
 				if (IS_LAYER_ON(NAV)) layer_off(NAV);
+				if (IS_LAYER_OFF(NAV) && (timer_read() - nav_layer_timer) < TAPPING_TERM ) {
+					tap_code(KC_ESC);
+				}
+				nav_layer_timer = 0;
 			}
 			return false;
 		case SYM_HOLD:
 			if (record->event.pressed) {
+				sym_layer_timer = timer_read();
 				if (IS_LAYER_ON(MEDIA)) layer_off(MEDIA);
 				if (IS_LAYER_OFF(SYM)) layer_on(SYM);
 			} else {
 				unselect_app_selection();
 				if (IS_LAYER_ON(SYM)) layer_off(SYM);
+				if (IS_LAYER_OFF(SYM) && (timer_read() - sym_layer_timer) < TAPPING_TERM) {
+					tap_code(KC_BSPC);
+				}
+				sym_layer_timer = 0;
 			}
 			return false;
 		case HIS_BACK:
