385e61d4c5ccfdd9efb17dbe519df56c70c174fc
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2024-12-13 09:29:40 +0300

    update: sync with zmk config

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # QMK 3x5+3
 
 ## Based on `crkbd` keyboard.
-![5x3+3](https://github.com/user-attachments/assets/4237be79-3ffc-4a09-b1b1-bef819551635)
+![5x3+3](https://github.com/user-attachments/assets/01e0d3d7-863e-41fc-908d-41bf577879d8)
--- chocolad_rgb/keymaps/strash/keymap.c
+++ chocolad_rgb/keymaps/strash/keymap.c
@@ -28,19 +28,15 @@ enum key {
 	HIS_FORW,          // go forward in the history (browser)
 	PREV_TAB,          // go to the previous tab
 	NEXT_TAB,          // go to the next tab
-	PREV_APP,          // go to the previous app
-	NEXT_APP,          // go to the next app
 	PREW_SPC,          // go to the previous space
 	NEXT_SPC,          // go to the next space
 	WEB_INSPECTOR,     // open/close web inspector
-    C_MEDIA,           // close media
 };
 
 // tap dance
 
 enum tap_dance {
 	TILD_GRV,
-	MINS_PLUS,
 	CIRC_BSLS,
 	QWE_P_QUOT,
 	QWE_M_LBRC,
@@ -50,7 +46,6 @@ enum tap_dance {
 
 tap_dance_action_t tap_dance_actions[] = {
 	[TILD_GRV]      = ACTION_TAP_DANCE_DOUBLE(KC_TILD, KC_GRV),
-	[MINS_PLUS]     = ACTION_TAP_DANCE_DOUBLE(KC_MINS, KC_PLUS),
 	[CIRC_BSLS]     = ACTION_TAP_DANCE_DOUBLE(KC_CIRC, KC_BSLS),
 	[QWE_P_QUOT]    = ACTION_TAP_DANCE_DOUBLE(KC_P,    KC_QUOT),
 	[QWE_M_LBRC]    = ACTION_TAP_DANCE_DOUBLE(KC_M,    KC_LBRC),
@@ -63,14 +58,20 @@ tap_dance_action_t tap_dance_actions[] = {
 enum combo_event {
 	DVO_CAPS_WORD,
 	QWE_CAPS_WORD,
+	MEDIA_TOG_WITH_MOD,
+	MEDIA_TOG,
 };
 
 const uint16_t PROGMEM dvo_caps_word_combo[] = {LSFT_T(KC_E), RSFT_T(KC_T), COMBO_END};
 const uint16_t PROGMEM qwe_caps_word_combo[] = {LSFT_T(KC_D), RSFT_T(KC_K), COMBO_END};
+const uint16_t PROGMEM media_w_mod_combo[] = {LCTL_T(KC_SPC), RCTL_T(KC_ENT), COMBO_END};
+const uint16_t PROGMEM media_combo[] = {KC_SPC, KC_ENT, COMBO_END};
 
 combo_t key_combos[] = {
 	[DVO_CAPS_WORD] = COMBO(dvo_caps_word_combo, CW_TOGG),
 	[QWE_CAPS_WORD] = COMBO(qwe_caps_word_combo, CW_TOGG),
+	[MEDIA_TOG_WITH_MOD] = COMBO(media_w_mod_combo, TG(MEDIA)),
+	[MEDIA_TOG] = COMBO(media_combo, TG(MEDIA)),
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { \
@@ -95,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { \
 	//|-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+-------------|
 	            KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,            KC_Y,         KC_U,         KC_I,         KC_O,TD(QWE_P_QUOT), \
 	//|-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+-------------|
-	            KC_A,         KC_S, LSFT_T(KC_D),         KC_F,         KC_G,            KC_H,         KC_J, RSFT_T(KC_K),         KC_L,      KC_SCLN, \
+	            KC_A, LOPT_T(KC_S), LSFT_T(KC_D), LCMD_T(KC_F),         KC_G,            KC_H, RCMD_T(KC_J), RSFT_T(KC_K), ROPT_T(KC_L),      KC_SCLN, \
 	//|-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+-------------|
 	            KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,            KC_N,TD(QWE_M_LBRC),TD(QWE_COMM_RBRC),  KC_DOT,TD(QWE_SLSH_BSLS), \
 	//|-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+-------------|
@@ -109,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { \
 	//|-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+-------------|
 	    LSFT(KC_TAB),       KC_TAB,      KC_LSFT,      KC_LCMD,LOPT(KC_BSPC),   RCMD(KC_LEFT),      KC_LEFT,      KC_DOWN,        KC_UP,      KC_RGHT, \
 	//|-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+-------------|
-	        PREV_APP,     NEXT_APP,     PREW_SPC,     NEXT_SPC, LOPT(KC_DEL),    RSA(KC_LEFT),ROPT(KC_LEFT),ROPT(KC_DOWN),  ROPT(KC_UP),ROPT(KC_RGHT), \
+	            KC_Q,      XXXXXXX,     PREW_SPC,     NEXT_SPC, LOPT(KC_DEL),    RSA(KC_LEFT),ROPT(KC_LEFT),ROPT(KC_DOWN),  ROPT(KC_UP),ROPT(KC_RGHT), \
 	//|-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+-------------|
 									     XXXXXXX,      XXXXXXX,      XXXXXXX,            LANG,         BASE,      XXXXXXX \
 							    //|-------------+-------------+-------------|  |-------------+-------------+-------------|
@@ -121,9 +122,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { \
 	//|-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+-------------|
 	           KC_P1,        KC_P2,        KC_P3,        KC_P4,        KC_P5,           KC_P6,        KC_P7,        KC_P8,        KC_P9,        KC_P0, \
 	//|-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+-------------|
-	         KC_LCBR,      KC_RCBR,TD(MINS_PLUS),       KC_EQL, TD(TILD_GRV),         KC_SLSH,      KC_LBRC,      KC_RBRC,      KC_LPRN,      KC_RPRN, \
+	         KC_LCBR,      KC_RCBR,      KC_MINS,       KC_EQL, TD(TILD_GRV),         KC_SLSH,      KC_LBRC,      KC_RBRC,      KC_LPRN,      KC_RPRN, \
 	//|-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+-------------|
-									     XXXXXXX,      KC_UNDS,    TO(MEDIA),         XXXXXXX,      XXXXXXX,      XXXXXXX \
+									     XXXXXXX,      KC_UNDS,      KC_PLUS,         XXXXXXX,      XXXXXXX,      XXXXXXX \
 							    //|-------------+-------------+-------------|  |-------------+-------------+-------------|
 	),
 
@@ -135,36 +136,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { \
 	//|-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+-------------|
 	         KC_LEFT,      KC_DOWN,        KC_UP,      KC_RGHT,   LCMD(KC_V),         XXXXXXX,      KC_LEFT,      KC_DOWN,        KC_UP,      KC_RGHT, \
 	//|-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+-------------|
-										 XXXXXXX,      C_MEDIA,       KC_SPC,          KC_ENT,      C_MEDIA,      XXXXXXX \
+										 XXXXXXX,    TG(MEDIA),       KC_SPC,          KC_ENT,    TG(MEDIA),      XXXXXXX \
 							    //|-------------+-------------+-------------|  |-------------+-------------+-------------|
 	),
 };
 
-bool is_select_prev_app_active = false;
-bool is_select_next_app_active = false;
-
-void unselect_app_selection(void) {
-	// unselect PREV_APP
-	if (is_select_prev_app_active == true) {
-		is_select_prev_app_active = false;
-		unregister_mods(MOD_MASK_SG);
-	}
-	// unselect NEXT_APP
-	if (is_select_next_app_active == true) {
-		is_select_next_app_active = false;
-		unregister_mods(MOD_MASK_GUI);
-	}
-};
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-	static bool is_nav_interrupted = false;
-	static bool is_sym_interrupted = false;
-
-	if (record->event.pressed) {
-		is_nav_interrupted = true;
-		is_sym_interrupted = true;
-	}
-
     switch (keycode) {
 		case BASE:
 			if (record->event.pressed) {
@@ -174,28 +151,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 				clear_keyboard();
 			}
 			return false;
-		case NAV_HOLD:
-			if (record->event.pressed) {
-				is_nav_interrupted = false;
-			} else {
-				unselect_app_selection();
-				if (is_nav_interrupted && IS_LAYER_ON(NAV)) {
-					layer_off(NAV);
-					return false;
-				}
-			}
-			return true;
-		case SYM_HOLD:
-			if (record->event.pressed) {
-				is_sym_interrupted = false;
-			} else {
-				unselect_app_selection();
-				if (is_sym_interrupted && IS_LAYER_ON(SYM)) {
-					layer_off(SYM);
-					return false;
-				}
-			}
-			return true;
 		case HIS_BACK:
 			if (record->event.pressed) {
 				register_mods(MOD_MASK_GUI);
@@ -228,36 +183,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 				clear_keyboard();
 			}
 			return false;
-		case PREV_APP:
-			if (record->event.pressed) {
-				if (is_select_prev_app_active == false) {
-					if (is_select_next_app_active == true) {
-						is_select_next_app_active = false;
-						clear_mods();
-					}
-					register_mods(MOD_MASK_SG);
-					tap_code(KC_TAB);
-					is_select_prev_app_active = true;
-				} else {
-					tap_code(KC_TAB);
-				}
-			}
-			return false;
-		case NEXT_APP:
-			if (record->event.pressed) {
-				if (is_select_next_app_active == false) {
-					if (is_select_prev_app_active == true) {
-						is_select_prev_app_active = false;
-						clear_mods();
-					}
-					register_mods(MOD_MASK_GUI);
-					tap_code(KC_TAB);
-					is_select_next_app_active = true;
-				} else {
-					tap_code(KC_TAB);
-				}
-			}
-			return false;
 		case PREW_SPC:
 			if (record->event.pressed) {
 				register_mods(MOD_MASK_CTRL);
@@ -281,9 +206,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 			} else {
 				clear_keyboard();
 			}
-			return false;
-		case C_MEDIA:
-			if (IS_LAYER_ON(MEDIA)) layer_off(MEDIA);
 			return false;
 	}
 	return true;
